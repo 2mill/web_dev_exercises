eb8a9ad36a6ef40399d22527ddae4c61069b5f59
Yannick Dorn <yannick.dorn@gmail.com>
2020-01-14 23:20:29 -0500

    need to fix some issues with eventListener

--- web-dev-practice/color_game_project/color-game.js
+++ web-dev-practice/color_game_project/color-game.js
@@ -14,13 +14,22 @@ let randomColors = (count) => {
     return colors;
 }
 let colors = randomColors(6);
-console.log(colors);
+let pickedColor = Math.floor(Math.random() * colors.length);
 var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("color-display");
+colorDisplay.textContent = colors[pickedColor];
 let used = [];
 for (var i = 0; i < squares.length; i++) {
     let rand = Math.floor(Math.random() * 6);
     while (used[rand]) rand = Math.floor(Math.random() * 6);
     used[rand] = true;
     squares[i].style.backgroundColor = colors[rand];
+    squares[i].addEventListener("click", () => {
+        let clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            alert("correct color");
+        } else {
+            alert("wrong color");
+        }
+    });
 }
-let question = document.getElementsByClassName("question");
