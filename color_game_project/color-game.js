1dc6ff63dc6c3a4655dbeb96ed92d48fab79c3ae
sw-Yannick Dorn <sw-ydorn@fsu87533.frostburg.edu>
2020-01-16 09:59:03 -0500

    Got the base functionality back after cleaning up the code

--- color_game_project/color-game.js
+++ color_game_project/color-game.js
@@ -16,58 +16,40 @@ let changeAllBlocksToColor = (color, selector)=> {
         selector[i].style.backgroundColor = color; 
     }
 }
-// let setup = (count) => {
-//     let colors = randomColors(6);
-//     console.log(colors);
-//     pickedColor = Math.floor(Math.random() * colors.length);
-//     let squares = document.querySelectorAll(".square");
-//     let messageDisplay = document.querySelector("#message");
-//     var colorDisplay = document.getElementById("color-display");
-//     let h1 = document.querySelector("h1");
-//     colorDisplay.textContent = colors[pickedColor];
-//     for (var i = 0; i < squares.length; i++) {
-//         console.log("adding " + colors[i] + " to square");
-//         squares[i].style.backgroundColor= colors[i];
-
-//         squares[i].addEventListener("click", function() {
-//             let clickedColor = this.style.backgroundColor;
-//             if (clickedColor === colors[pickedColor]) {
-//                 messageDisplay.textContent = "Correct";
-//                 changeAllBlocksToColor(this.style.backgroundColor, squares);
-//                 h1.style.backgroundColor = this.style.backgroundColor;
-//             } else {
-//                 this.style.backgroundColor= "#232323"
-//                 messageDisplay.textContent = "Try Again";
-//             }
-//         });
-//     }
-//     return true;
-// } 
 let createGame = (difficulty) => {
    let container = document.querySelector("#container");
    colors = randomColors(difficulty);
    let pickedColor = Math.floor(Math.random() * colors.length);
+   setDisplays(colors[pickedColor]);
    for (let i = 0; i < colors.length; i++) {
        let square = document.createElement("div");
        square.className = "square";
        square.style.backgroundColor = colors[i]; 
+       let msg = document.querySelector("#message");
        square.addEventListener("click", () => {
-           console.log("cllicked a square");
            if (square.style.backgroundColor === colors[pickedColor]) {
-               console.log("yes");
+               msg.textContent = "Correct!";
                let squares = document.querySelectorAll(".square");
-               console.log(squares);
                for (let j = 0; j < squares.length; j++) {
                    squares[j].style.backgroundColor = square.style.backgroundColor;
                }
            } else {
-               console.log("no");
+              msg.textContent = "Wrong Answer";
            }
        });
        container.appendChild(square);
    }
    return true;
 }
+let setDisplays = (color) => {
+    document.querySelector("#message").textContent = "";
+
+    {
+        document.querySelector("#color-display").textContent = color;
+        document.querySelector("#color-display").style.color = color;
+    }
+}
+
 let clearBoard = () => {
     let container = document.querySelector("#container");
     let square = document.querySelectorAll(".square");
@@ -76,11 +58,12 @@ let clearBoard = () => {
     }
 }
 
-
-
-
+let startGame = () => {
+    createGame(6);
+}
 let resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", () => {
     clearBoard();
-    createGame(7);
+    createGame(6);
 });
+startGame();
