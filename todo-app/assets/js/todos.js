1e2df686c759953a4901f17f0443a4cd28ee4fba
sw-Yannick Dorn <sw-ydorn@fsu87533.frostburg.edu>
2020-01-17 12:24:38 -0500

    folder structure and js functionality

---
+++ todo-app/assets/js/todos.js
@@ -0,0 +1,19 @@
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+$("ul").on("click", "span", function () {
+    $(this).parent().fadeOut(500, () => $(this).remove());
+    event.stopPropagation();
+})
+
+$("li").on("click", "span", function () {
+    $(this).parent().fadeOut(500, () => $(this).remove());
+    event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
+})
